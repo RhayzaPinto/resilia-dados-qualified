2af8f2b8cad53e089dda7d9768e15c824f91d0bb
Rhayza Pinto <rhayzapinto@gmail.com>
2021-11-25 01:20:08 -0300

    Adicionando exercício ao repositório

---
+++ qualified_podio.py
@@ -0,0 +1,17 @@
+def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3):
+# Implemente aqui a lógica da função 
+  if tempo_chegada1 < tempo_chegada2 and tempo_chegada1 < tempo_chegada3:
+    if tempo_chegada2 < tempo_chegada3:
+      return f"1 - {tempo_chegada1} minutos\n2 - {tempo_chegada2} minutos\n3 - {tempo_chegada3} minutos\n"
+    else:
+      return f"1 - {tempo_chegada1} minutos\n2 - {tempo_chegada3} minutos\n3 - {tempo_chegada2} minutos\n"
+  elif tempo_chegada2 < tempo_chegada1 and tempo_chegada2 < tempo_chegada3:
+    if tempo_chegada1 < tempo_chegada3:
+      return  f"1 - {tempo_chegada2} minutos\n2 - {tempo_chegada1} minutos\n3 - {tempo_chegada3} minutos\n"
+    else:
+      return f"1 - {tempo_chegada2} minutos\n2 - {tempo_chegada3} minutos\n3 - {tempo_chegada1} minutos\n"
+  elif tempo_chegada3 < tempo_chegada1 and tempo_chegada3 < tempo_chegada2:
+    if tempo_chegada1 < tempo_chegada2:
+      return f"1 - {tempo_chegada3} minutos\n2 - {tempo_chegada1} minutos\n3 - {tempo_chegada2} minutos\n"
+    else:
+      return f"1 - {tempo_chegada3} minutos\n2 - {tempo_chegada2} minutos\n3 - {tempo_chegada1} minutos\n"
